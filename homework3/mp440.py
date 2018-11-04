57d400eaa5bd54c563aabb640ea1f69e7d68137e
yoni <yoniifriedman@gmail.com>
2018-11-04 12:05:09 -0500

    fixed bugs and finished alpha beta pruning

--- homework3/mp440.py
+++ homework3/mp440.py
@@ -15,12 +15,12 @@ here that row and column both start with index 0.
 def check_up(state, player, row, column):
     opponent = 'W' if player == 'B' else 'B'
     flipped = 0
-    i = row + 1
+    i = row - 1
     j = column
-    while i < len(state):
+    while i >= 0:
         if state[i][j] == opponent:
             flipped += 1
-            i += 1 
+            i -= 1 
         #No sandwiching player piece:
         elif state[i][j] == ' ': 
             return 0 
@@ -31,12 +31,12 @@ def check_up(state, player, row, column):
 def check_down(state, player, row, column):
     opponent = 'W' if player == 'B' else 'B'
     flipped = 0
-    i = row - 1
+    i = row + 1
     j = column
-    while i >= 0:
+    while i < len(state):
         if state[i][j] == opponent:
             flipped += 1
-            i -= 1 
+            i += 1 
         elif state[i][j] == ' ': 
             return 0 
         elif state[i][j] == player:
@@ -129,7 +129,7 @@ def check_diagonal_top_left(state, player, row, column):
     flipped = 0
     i = row - 1
     j = column - 1
-    while i >= 0 and j < len(state[i]):
+    while i >= 0 and j >= 0:
         if state[i][j] == opponent:
             flipped += 1
             i -= 1 
@@ -147,6 +147,7 @@ def get_move_value(state, player, row, column):
     total_flipped += check_up(state, player, row, column)
     total_flipped += check_down(state, player, row, column)
     total_flipped += check_right(state, player, row, column)
+    total_flipped += check_left(state, player, row, column)
     total_flipped += check_diagonal_top_right(state, player, row, column)
     total_flipped += check_diagonal_top_left(state, player, row, column)
     total_flipped += check_diagonal_bottom_right(state, player, row, column)
@@ -162,6 +163,9 @@ in particular, should not share memory with the old state.
 def execute_move(old_state, player, row, column):
     if row > len(old_state) or row < 0 or column > len(old_state[0]) or column < 0:
         return None
+    
+    if old_state[row][column] != ' ':
+        return None
 
     state = deepcopy(old_state)
     state[row][column] = player
@@ -171,12 +175,12 @@ def execute_move(old_state, player, row, column):
     #Check if you gain points flipping pieces above
     if check_up(state, player, row, column) > 0:
         #Up
-        i = row + 1
+        i = row - 1
         j = column
-        while i < len(state):
+        while i >= 0:
             if state[i][j] == opponent:
                 state[i][j] = player
-                i += 1 
+                i -= 1 
             #No sandwiching player piece:
             elif state[i][j] == ' ': 
                 break
@@ -185,12 +189,12 @@ def execute_move(old_state, player, row, column):
     
     #Check if any points are gained flipping pieces below in the row
     if check_down(state, player, row, column) > 0:
-        i = row - 1
+        i = row + 1
         j = column
-        while i >= 0:
+        while i < len(state):
             if state[i][j] == opponent:
                 state[i][j] = player
-                i -= 1 
+                i += 1 
             elif state[i][j] == ' ': 
                 break
             elif state[i][j] == player:
@@ -328,41 +332,82 @@ def is_terminal_state(state, state_list = None):
 The minimax algorithm. Your implementation should return the best value for the
 given state and player, as well as the next immediate move to take for the player. 
 '''
-def minimax(state, player, d=0):
-    print 'iteration: ' + str(d) + ":", 'player', player
+def minimax(state, player, move_list):
+    '''
+    print
+    print player + " to move:"
     for i in range(len(state)):
-        for j in range(len(state[0])):
-            print (state[i][j]), 
-        print
-
-    print('------------------')
-
+            print state[i]
+    print
+'''
+    
     if is_terminal_state(state):
+    #    print "Terminal State reached"
+        
         (blackpieces, whitepieces) = count_pieces(state)
         value = blackpieces - whitepieces
-        return (value, (-1, -1))
+        move_list.append((player, -1,-1))
+    #    print "\t Returning: " + str(value), move_list
+        return (value, move_list)
     
-    value = 0
+    best_move = []
     if player == 'B':
+        value = -10000000
+        found_a_move = False
         for i in range(len(state)):
-            for j in range(len(state[i])):
+            for j in range(len(state)):
+                val = get_move_value(state, 'B', i, j)
+    #            print
+    #            print 'Piece at (' + str(i) + ', ' + str(j) + '): ', state[i][j]
+    #            print 'Value of moving there for Black: ' + str(val)
+                if state[i][j] == ' ' and val > 0: 
+    #                print 'TAKING SQUARE AT', i, j
+                    found_a_move = True
+                    possible_state = execute_move(state, 'B', i, j)     #Update state
+                    possible_moves = move_list[:]                       #Update list of moves
+                    possible_moves.append(('B', i, j))
+                    (possible_value, possible_moves) = minimax(possible_state, 'W', possible_moves) 
+    #                print
+    #                print "Black evaluating Move: possible_value = ", possible_value, "\t current value = ", value
 
-                if state[i][j] == ' ' and get_move_value(state, 'B', i, j) > 0:  
-                    possible_state = execute_move(state, 'B', i, j) 
-                    (possible_value, moves) = minimax(possible_state, 'W', d+1) 
-                    value = max(possible_value, value)
-        return (value, (-1, -1))
+                    if possible_value > value:
+    #                    print "Swapping move for Black to: ",
+                        (value, best_move) = (possible_value, possible_moves)
+    #                    print(value, best_move)
+        
+        if found_a_move == False:          # We're not in terminal state but white can't move - Black moves again
+            return minimax(state, 'W', move_list) 
+        return (value, best_move)
 
     if player == 'W':
+        value = 1000000
+        found_a_move = False
         for i in range(len(state)):
-            for j in range(len(state[i])):
+            for j in range(len(state)):
+                val = get_move_value(state, 'W', i, j)
+    #            print 
+    #            print 'Piece at (' + str(i) + ', ' + str(j) + '): ', state[i][j]
+    #            print 'Value of moving there for White: ' + str(val)
+               
+                if state[i][j] == ' ' and val > 0: 
+    #                print "TAKING SQUARE AT ", i, j
+                    found_a_move = True
+                    possible_state = execute_move(state, 'W', i, j)     #Update state
+                    possible_moves = move_list[:]                       #Update list of moves
+                    possible_moves.append(('W', i, j))
+                    (possible_value, possible_moves) = minimax(possible_state, 'B', possible_moves) 
+    #                print
+    #                print "White evaluating Move: possible_value = ", possible_value, "\t current value = ", value
 
-                if state[i][j] == ' ' and get_move_value(state, 'W', i, j) > 0: 
-                    possible_state = execute_move(state, 'W', i, j) 
-                    (possible_value, moves) = minimax(possible_state, 'B', d+1) 
-                    value = min(possible_value, value)
+                    if possible_value < value:
+    #                    print "Swapping move for White to: ",
+                        (value, best_move) = (possible_value, possible_moves)
+    #                    print(value, best_move)
         
-        return (value, (-1, -1))
+        if found_a_move == False:          # We're not in terminal state but white can't move - Black moves again
+            return minimax(state, 'B', move_list) 
+       
+        return (value, best_move)
 '''
 This method should call the minimax algorithm to compute an optimal move sequence
 that leads to an end game. 
@@ -370,8 +415,8 @@ that leads to an end game.
 def full_minimax(state, player):
     value = 0
     move_sequence = []
-    (value) = minimax(state, player)
-    return (value)
+    (value, move_sequence) = minimax(state, player, [])
+    return (value, move_sequence)
 
 
 '''
@@ -379,7 +424,89 @@ The minimax algorithm with alpha-beta pruning. Your implementation should return
 best value for the given state and player, as well as the next immediate move to take
 for the player. 
 '''
-def minimax_ab(state, player, alpha = -10000000, beta = 10000000):
+def minimax_ab(state, player, move_list, alpha, beta):
+    '''   
+    print
+    print player + " to move:"
+    for i in range(len(state)):
+            print state[i]
+    print
+    '''
+    if is_terminal_state(state):
+    #    print "Terminal State reached"
+        
+        (blackpieces, whitepieces) = count_pieces(state)
+        value = blackpieces - whitepieces
+        move_list.append((player, -1,-1))
+    #    print "\t Returning: " + str(value), move_list
+        return (value, move_list)
+    
+    best_move = []
+    if player == 'B':
+        value = -10000000
+        found_a_move = False
+        for i in range(len(state)):
+            for j in range(len(state)):
+                val = get_move_value(state, 'B', i, j)
+    #            print
+    #            print 'Piece at (' + str(i) + ', ' + str(j) + '): ', state[i][j]
+    #            print 'Value of moving there for Black: ' + str(val)
+                if state[i][j] == ' ' and val > 0: 
+    #                print 'TAKING SQUARE AT', i, j
+                    found_a_move = True
+                    possible_state = execute_move(state, 'B', i, j)     #Update state
+                    possible_moves = move_list[:]                       #Update list of moves
+                    possible_moves.append(('B', i, j))
+                    (possible_value, possible_moves) = minimax_ab(possible_state, 'W', possible_moves, alpha, beta) 
+    #                print
+    #                print "Black evaluating Move: possible_value = ", possible_value, "\t current value = ", value
+
+                    if possible_value > value:
+    #                    print "Swapping move for Black to: ",
+                        (value, best_move) = (possible_value, possible_moves)
+                        if value > beta:                                #Pruning
+                            return (value, best_move)   
+    #                    print(value, best_move)        
+
+                    alpha = max(alpha, value)                           #Update
+        
+        if found_a_move == False:          # We're not in terminal state but white can't move - Black moves again
+            return minimax_ab(state, 'W', move_list, alpha, beta) 
+        return (value, best_move)
+
+    if player == 'W':
+        value = 1000000
+        found_a_move = False
+        for i in range(len(state)):
+            for j in range(len(state)):
+                val = get_move_value(state, 'W', i, j)
+    #            print 
+    #            print 'Piece at (' + str(i) + ', ' + str(j) + '): ', state[i][j]
+    #            print 'Value of moving there for White: ' + str(val)
+               
+                if state[i][j] == ' ' and val > 0: 
+    #                print "TAKING SQUARE AT ", i, j
+                    found_a_move = True
+                    possible_state = execute_move(state, 'W', i, j)     #Update state
+                    possible_moves = move_list[:]                       #Update list of moves
+                    possible_moves.append(('W', i, j))
+                    (possible_value, possible_moves) = minimax_ab(possible_state, 'B', possible_moves, alpha, beta) 
+    #                print
+    #                print "White evaluating Move: possible_value = ", possible_value, "\t current value = ", value
+
+                    if possible_value < value:
+    #                    print "Swapping move for White to: ",
+                        (value, best_move) = (possible_value, possible_moves)
+                        if value <= alpha:                              #Pruning
+                            return (value, best_move)
+    #                    print(value, best_move)
+
+                    beta = min(beta, value)                             #Update
+                    
+        if found_a_move == False:          # We're not in terminal state but white can't move - Black moves again
+            return minimax_ab(state, 'B', move_list, alpha, beta) 
+       
+        return (value, best_move)
     value = 0
     row = -1
     column = -1
@@ -393,7 +520,7 @@ that leads to an end game, using alpha-beta pruning.
 def full_minimax_ab(state, player):
     value = 0
     move_sequence = []
-    # Your implementation goes here 
+    (value, move_sequence) = minimax_ab(state, player, [], -10000000, 10000000)
     return (value, move_sequence)
 
 
