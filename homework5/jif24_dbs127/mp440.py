e1b0d1559b851727fa6eee1121e3300cb3e6ddff
Yoni <yoniifriedman@gmail.com>
2018-12-08 19:38:44 -0500

    Ghost Hunter 2.0

--- homework5/jif24_dbs127/mp440.py
+++ homework5/jif24_dbs127/mp440.py
@@ -31,27 +31,26 @@ def kf_update(H, P, X, R, z):
 '''
 Kalman 2D
 '''
-def kalman2d(data):
+def kalman2d(data, lambd = 1.5):
     estimated = []
     A = np.identity(2)
     B = np.identity(2)
     H = np.identity(2)
-    Q = np.array([[0.0001, 0.00002], [ 0.00002,0.0001]])    #Covariance matrix for zero mean Gaussian system noise w
-    R = np.array([[0.01, 0.005], [0.005, 0.02]])            #Covariance matrix for zero mean Gaussian measurement noise 
+    Q = np.matrix([[0.0001, 0.00002], [ 0.00002,0.0001]])    #Covariance matrix for zero mean Gaussian system noise w
+    R = np.matrix([[0.01, 0.005], [0.005, 0.02]])            #Covariance matrix for zero mean Gaussian measurement noise 
     
     #Initial estimates
-    P_prev = np.multiply(.5, np.identity(2))
-    X = np.array([0, 0])                 #Initial position estimated as first measurement
+    P_prev = lambd * np.identity(2)
+    X = transpose(np.matrix([0, 0]))                #Initial position estimated as first measurement
     estimated.append(X)
     i = 0
     for d in data:
-        u = np.array([d[0], d[1]])
-        z = np.array([d[2], d[3]])
+        u = transpose(np.matrix([d[0], d[1]]))
+        z = transpose(np.matrix([d[2], d[3]]))
         x = estimated[i]
         P = P_prev
         (x_predicted, p_predicted) = kf_predict(A, x, B, u, P, Q)
         (x_updated, p_updated) = kf_update(H, p_predicted, x_predicted, R, z)
-        x_updated = [x_updated[0][0], x_updated[1][1]]
         P_prev = p_updated
         estimated.append(x_updated)
         i += 1
@@ -70,11 +69,12 @@ def plot(data, output):
         x_data.append(d[2])
         y_data.append(d[3])
     for o in output:
-        x_output.append(o[0])
-        y_output.append(o[1])
+        #print o.item(0), o.item(1)
+        x_output.append(o.item(0))
+        y_output.append(o.item(1))
     
-    l1 = plt.plot(x_data, y_data, label="Data Received")
-    l2 = plt.plot(x_output, y_output, label="Predicted Position")
+    l1 = plt.plot(x_data, y_data, label="Observations")
+    l2 = plt.plot(x_output, y_output, label="Estimated Position")
     plt.setp(l1, color='Blue')
     plt.setp(l2, color='Red')
     plt.legend()
@@ -84,12 +84,42 @@ def plot(data, output):
 '''
 Kalman 2D 
 '''
+P0 =  2 * np.identity(2)
+ghost_tracks = [[0,0]]
+error_estimates = [P0]
 def kalman2d_shoot(ux, uy, ox, oy, reset=False):
-    decision = (0, 0, False)
-    # Your code starts here 
-    # Your code ends here 
-    _raise_not_defined()
-    return decision
+    global P0
+    global ghost_tracks
+    global error_estimates
+
+    if reset == True:
+        P0 =  2 * np.identity(2)
+        ghost_tracks = [[0,0]]
+        error_estimates = [P0]
+
+    A = np.identity(2)
+    B = np.identity(2)
+    H = np.identity(2)
+    Q = np.matrix([[0.0001, 0.00002], [ 0.00002,0.0001]])    #Covariance matrix for zero mean Gaussian system noise w
+    R = np.matrix([[0.01, 0.05], [0.05, 0.02]])            #Covariance matrix for zero mean Gaussian measurement noise 
+    u = transpose([ux, uy])
+    z = transpose([ox, oy])
+    
+    (x_pred, p_pred) = kf_predict(A, ghost_tracks.pop(), B, u, error_estimates.pop(), Q)
+    (x_updated, p_updated) = kf_update(H, p_pred, x_pred, R, z)
+    
+    print p_updated
+    print 
+    print
+    if p_updated.item(0) < .0015 and p_updated.item(2) < 0.014:
+        return ((x_updated.item(0), x_updated.item(1), True))
+    
+    ghost_tracks.append(x_updated)
+    error_estimates.append(p_updated)
+
+    return ((x_updated.item(0), x_updated.item(1), False))
+
+    
 
 '''
 Kalman 2D 
